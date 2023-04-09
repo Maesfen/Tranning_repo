2d5c67132378ba9d2ab2b80e2c4766aed2e4ee30
Esubalew <122857025+Maesfen@users.noreply.github.com>
2023-04-09 14:43:56 +0300

    Update lists.h

--- lists.h
+++ lists.h
@@ -2,12 +2,12 @@
 #define LISTS_H
 
 /*
-* Desc: header file containinf prototypes and definitions
-*	for all functions and types written in this proj
-*/
+ * File: lists.h
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x11-singly_linked_lists directory.
+ */
 
 #include <stdlib.h>
-#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -16,7 +16,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *              for Holberton project
+ *              for Alx project
  */
 typedef struct list_s
 {
@@ -31,4 +31,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* LISTS_H */
